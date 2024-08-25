e1fe474cdd83b50ee6d37627a7f3a227df0190f5
FC (Fay) Stegerman <flx@obfusk.net>
2024-08-25 02:56:09 +0200

    ziptools.py: EOCD

--- ziptools.py
+++ ziptools.py
@@ -3,7 +3,12 @@
 # SPDX-FileCopyrightText: 2024 FC (Fay) Stegerman <flx@obfusk.net>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+# FIXME: zip64, ...
+
+from __future__ import annotations
+
 import os
+import struct
 
 from dataclasses import dataclass
 from typing import BinaryIO, Union
@@ -18,11 +23,37 @@ class Error(Exception):
 
 
 @dataclass(frozen=True)
+class EOCD:
+    """End of central directory record."""
+    signature: bytes            # 4
+    disk_number: int            # 2
+    cd_start_disk: int          # 2
+    num_cd_records_disk: int    # 2
+    num_cd_records_total: int   # 2
+    cd_size: int                # 4
+    cd_offset: int              # 4
+    comment_len: int            # 2
+    comment: bytes              # comment_len
+
+    @classmethod
+    def parse(_cls, data: bytes) -> EOCD:
+        signature = data[:4]
+        if signature != EOCD_SIGNATURE:
+            raise Error("Expected end of central directory record (EOCD)")
+        (disk_number, cd_start_disk, num_cd_records_disk, num_cd_records_total,
+            cd_size, cd_offset, comment_len) = struct.unpack("<HHHHIIH", data[4:22])
+        comment = data[22:]
+        return _cls(signature, disk_number, cd_start_disk, num_cd_records_disk,
+                    num_cd_records_total, cd_size, cd_offset, comment_len, comment)
+
+
+@dataclass(frozen=True)
 class CentralDirectory:
     """Central directory including EOCD."""
     cd_offset: int
     eocd_offset: int
     cd_data: bytes
+    eocd_data: bytes
 
 
 def read_cd(zipfile: Union[BinaryIO, str], count: int = 1024) -> CentralDirectory:
@@ -43,8 +74,9 @@ def _read_cd(fh: BinaryIO, count: int) -> CentralDirectory:
             fh.seek(16, os.SEEK_CUR)
             cd_offset = int.from_bytes(fh.read(4), "little")
             fh.seek(cd_offset)
-            cd_data = fh.read()
-            return CentralDirectory(cd_offset, eocd_offset, cd_data)
+            cd_data = fh.read(eocd_offset - cd_offset)
+            eocd_data = fh.read()
+            return CentralDirectory(cd_offset, eocd_offset, cd_data, eocd_data)
     raise Error("Expected end of central directory record (EOCD)")
 
 
