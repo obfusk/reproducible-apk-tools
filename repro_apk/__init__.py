dc069dc4cddf6ab5162f3ed3be1bc8a14711273f
FC (Fay) Stegerman <flx@obfusk.net>
2024-10-17 00:15:25 +0200

    v0.3.0

--- repro_apk/__init__.py
+++ repro_apk/__init__.py
@@ -28,7 +28,7 @@ from . import zipinfo as _zipinfo
 
 import click
 
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 NAME = "repro-apk"
 
 ERRORS = (
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import setuptools
 
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 
 info = Path(__file__).with_name("README.md").read_text(encoding="utf8")
 
